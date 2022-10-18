1f851c993a3bba94296392907ae8a064e57c6420
Rahmawati <115470125+Rhmawthy@users.noreply.github.com>
2022-10-18 04:08:58 -0700

    Add files via upload

---
+++ day8_Operator perbandingan.py
@@ -0,0 +1,37 @@
+#operator perbandigan
+#operator perbandigan dipakai untuk membandingkan 2 buah nilai,
+#apakah nilai tersebut sama besar,dst.
+#Hasil perbandingan ini adalah boolean True dan False.
+
+#Di dalam bahasa phyton, terdapat 6 operator perbandingan, yaitu:
+
+a = 5
+b = 6
+
+# Sama dengan (==)
+
+print( a == b )
+
+# Tidak sama dengan ( != )
+
+print ( a != b )
+
+# Lebih Besar ( > )
+
+print ( a > b )
+
+# Lebih kecil ( < )
+
+print ( a < b )
+
+# Lebih Besar atau sama dengan ( >= )
+
+print ( a >= b  )
+
+# Lebih kecil atau sama dengan ( <= )
+
+print ( a <= b )
+
+
+
+
